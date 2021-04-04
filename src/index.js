c392ad397cb2a4c2dec0c381950e23165e7886ff
Gustavo Oliveira <gmacedo@mcontigo.com>
2021-04-04 07:57:03 -0300

    fix: all tests pass

--- src/index.js
+++ src/index.js
@@ -10,19 +10,79 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.filter(user => user.username === username);
+
+  if (!user.length) {
+    return response.status(404).json({
+      error: 'User not found!'
+    });
+  }
+
+  request.user = user[0];
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({
+      error: 'User not allowed!'
+    });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  if (!validate(id)) {
+    return response.status(400).json({
+      error: 'ID not is valid!'
+    });
+  }
+
+  const user = users.filter(user => user.username === username);
+
+  if (!user.length) {
+    return response.status(404).json({
+      error: 'User not found!'
+    });
+  }
+
+  const todo = user[0].todos.filter(todo => todo.id === id);
+
+  if (!todo.length) {
+    return response.status(404).json({
+      error: 'Todo not found!'
+    });
+  }
+
+  request.todo = todo[0];
+  request.user = user[0];
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.filter(user => user.id === id);
+
+  if (!user.length) {
+    return response.status(404).json({
+      error: 'User not found!'
+    });
+  }
+
+  request.user = user[0];
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
